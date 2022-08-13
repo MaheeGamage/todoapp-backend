f2aea5c2f9f917a0ca67970f96e583cda637f035
MaheeGamage <mahee.gamage@gmail.com>
2022-08-13 16:33:22 +0530

    prisma ORM added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoTasksModule } from './todo-tasks/todo-tasks.module';
 
 @Module({
-  imports: [],
+  imports: [TodoTasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
